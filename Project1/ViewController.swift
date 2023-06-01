c2f8c8d228593968e89123560f7f2faff8564b88
Philip Nduka <philip.igboba@mkobo.com.ng>
2023-06-01 02:07:21 +0100

    added methods to TableController

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -7,11 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items{
+            if item.hasPrefix("nssl"){
+                //this is a picture to load
+                
+                pictures.append(item)
+            }
+        }
+          
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int{
+            return pictures.count
+        }
+        
+        
+        func tableView(_ tableView:UITableView, cellForRowAt indexPath: IndexPath )-> UITableViewCell{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for:indexPath )
+            cell.textLabel?.text = pictures[indexPath.row]
+            return cell
+        }
+        print(pictures)
     }
 
 
